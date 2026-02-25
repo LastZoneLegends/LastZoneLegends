e3dbbbaee64634f7bc0b38d6832f90e6f7a70932
LastZoneLegends <lastzonelegends0@gmail.com>
2026-02-25 07:59:37 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -22,9 +22,9 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-  body: payload.notification.body,
+const notificationTitle = payload.data.title;
+const notificationOptions = {
+  body: payload.data.body,
   icon: "/icons/icon-192x192.png",
   data: {
     url: payload.data?.url || "/"
@@ -49,4 +49,5 @@ self.addEventListener("notificationclick", function (event) {
     );
   }
 });
+
 
